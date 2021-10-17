af13ccc28a3a0f294ebb03ca11751f5783881282
rushirishi22 <92682622+rushirishi22@users.noreply.github.com>
2021-10-17 22:47:17 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -23,5 +23,14 @@ class StringCalculator {
         }
         return sum;
     }
+    
+    private int stringToInt(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+            throw new IllegalArgumentException("Negative input!");
+        } else {
+            return num;
+        }
+    }
 
 }
