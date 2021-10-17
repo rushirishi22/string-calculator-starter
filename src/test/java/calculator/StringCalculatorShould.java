66d0db96c6d08c361a0777181a07a0856def7fa4
rushirishi22 <92682622+rushirishi22@users.noreply.github.com>
2021-10-17 23:01:00 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -55,4 +55,9 @@ class StringCalculatorShould {
         stringCalculator.add("-5,10\n-15");
     }
     
+    @Test
+    public void numbersGreaterThan1000AreIgnored() {
+        assertEquals(stringCalculator.add("5,12,1001"), 17);
+        assertEquals(stringCalculator.add("14124,22\n4,1214"), 26);
+    }
 }
