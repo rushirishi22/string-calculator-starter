a267ed7b475efd4d3e51639a16cb952162ed1bdb
rushirishi22 <92682622+rushirishi22@users.noreply.github.com>
2021-10-17 22:45:06 +0530

    Create StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        String[] numbers = input.split(",|\n");
+
+        if (input.isEmpty()) {
+            return 0;
+        } else if (numbers.length > 1) {
+            return getSum(numbers);
+        }
+        return stringToInt(input);
     }
 
 }
